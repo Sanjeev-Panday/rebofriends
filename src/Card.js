9b2230ec78b4cb87970096ecc704f57f726908a5
Sanjeev Panday <sanjeev.pandey84@gmail.com>
2019-10-19 13:27:56 +0530

    Added Searchbox component and code to handle state

--- src/Card.js
+++ src/Card.js
@@ -4,8 +4,8 @@ import 'tachyons';
 const Card = (props)=>{
     const {name, email, id} = props;
     return (
-        <div className="tc bg-light-green dib br3 pa3 ma2 grow">
-            <img alt="photo" src={`https://robohash.org/${id}?size=200x200`}/>
+        <div className="tc grow bg-light-green br3 pa3 ma2 dib bw2 shadow-5">
+            <img alt={`photo_${id}`} src={`https://robohash.org/${id}?size=200x200`}/>
             <div>
                 <h2>{name}</h2>
                 <p>{email}</p>
--- src/index.js
+++ src/index.js
@@ -1,15 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Card from './Card.js';
+import App from './App';
 import * as serviceWorker from './serviceWorker';
-import {robots} from './robots';
 ReactDOM.render(
-<div>
-    <Card name={robots[0].name} email={robots[0].email} id={robots[0].id} />
-    <Card name={robots[1].name} email={robots[1].email} id={robots[1].id}/>
-    <Card name={robots[2].name} email={robots[2].email} id={robots[2].id}/>
-</div>
+<App />
 , document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
