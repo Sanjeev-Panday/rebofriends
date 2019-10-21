5f43b8a4e23e29730f56261574619dcb5a1746ae
Sanjeev Panday <sanjeev.pandey84@gmail.com>
2019-10-21 08:10:26 +0530

    Updates

---
+++ precache-manifest.c7de8ab5b640c0d66012df2df1f67fed.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "94d839406ad133ba3af8cad4b52dc772",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "2a7df483b8979bcb2ab4",
+    "url": "/robofriends/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "fedf6418bd034fb7b2bc",
+    "url": "/robofriends/static/css/main.a93d819d.chunk.css"
+  },
+  {
+    "revision": "2a7df483b8979bcb2ab4",
+    "url": "/robofriends/static/js/2.3cd86ebb.chunk.js"
+  },
+  {
+    "revision": "fedf6418bd034fb7b2bc",
+    "url": "/robofriends/static/js/main.e24f8827.chunk.js"
+  },
+  {
+    "revision": "69ae533dd49bc2dbc7cd",
+    "url": "/robofriends/static/js/runtime-main.9f59819d.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
